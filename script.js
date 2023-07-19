6833aaa0f3d8b2cbe9ff72e9d37ed1caaecc7a31
Aung Khant Paing <akpaing99@gmail.com>
2023-07-19 18:55:44 +0630

    complete the JS calculation parts

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function add(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 }
 function subtract(num1, num2) {
-  return num1 - num2;
+  return parseFloat(num1) - parseFloat(num2);
 }
 function multiply(num1, num2) {
-  return num1 * num2;
+  return parseFloat(num1) * parseFloat(num2);
 }
 function divide(num1, num2) {
-  return num1 / num2;
+  return parseFloat(num1) / parseFloat(num2);
 }
 
 const numBtns = document.querySelectorAll(".numBtn");
@@ -22,140 +22,83 @@ let num1;
 let num2;
 let resultNum;
 let array = [];
-let a = "";
+let a = "0";
 let b = "";
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if (btn.value != "=") {
-      inputNum.textContent += btn.value;
-    }
-  });
-});
-window.addEventListener("keydown", (e) => {
-  const key = document.querySelector(`.key[data-key=${e.code}]`);
-  const keyOp = document.querySelector(`.keyOp[data-key=${e.code}]`);
-  console.log(keyOp);
-  if (key && key.value != "=") {
-    inputNum.textContent += key.value;
-  }
-  if (!num1 && key) {
-    a += key.value;
-  } else if (num1 && !num2 && key) {
-    b += key.value;
-  }
-  if (array.length < 2 && inputNum.textContent) {
-    if (num1 && !num2) {
-      num2 = parseInt(b);
-      array[1] = num2;
-    } else if (!num1) {
-      num1 = parseInt(a);
-      array[0] = num1;
-    }
-  }
-  if (keyOp) {
-    arrayOp.push(keyOp.value);
-  }
+let isLastInputOperator = false;
 
-  console.log(array.length);
-  if (array.length > 1) {
-    operate();
-    num1 = parseInt(result.textContent);
-    num2 = null;
-    b = "";
-    array = [num1];
-    arrayOp = [];
-    console.log(array);
-  }
-  if (keyOp && keyOp.value == "=" && inputNum.textContent) {
-    inputNum.textContent = "";
-    a = "";
-    b = "";
-    num1 = null;
-    num2 = null;
-    array = [];
-    arrayOp = [];
-
-    key.addEventListener("click", () => {
-      result.textContent = "";
-    });
-  }
-  if (keyOp && keyOp.value == "clear") {
-    a = "";
-    b = "";
-    num1 = null;
-    num2 = null;
-    array = [];
-    arrayOp = [];
-    result.textContent = "";
-    inputNum.textContent = "";
-  }
-});
-// operatorBtns.forEach((operatorBtn) => {
-//   operatorBtn.addEventListener("click", () => {
-//     if (operatorBtn.value != "=") {
-//       operator.push(operatorBtn.value);
-//       console.log(operator);
-//       arrayNum = inputNum.textContent.split(/[+*/-]/g);
-//       arrayOp = operator.split("");
-//       console.log(arrayNum.length);
-//     }
-//     if (arrayNum.length > 2) {
-//       num1 = parseInt(arrayNum[0]);
-//       num2 = parseInt(arrayNum[1]);
-//       operate();
-//       resultNum = parseInt(result.textContent);
-//     }
-//     console.log(arrayNum);
-//   });
-// });
-
+// if the input is number store it in variable num1.if num1 already exist then store the input in num2 and call operate function
 numBtns.forEach((numBtn) => {
   numBtn.addEventListener("click", () => {
+    isLastInputOperator = false;
+    inputNum.textContent += numBtn.value;
     if (!num1) {
       a += numBtn.value;
-    } else if (num1 && !num2) {
+    } else if (num1) {
       b += numBtn.value;
+      num2 = b;
+      console.log(num2);
+      array[1] = num2;
+      operate();
     }
   });
 });
 operatorBtns.forEach((operatorBtn) => {
   operatorBtn.addEventListener("click", () => {
-    if (array.length < 2 && inputNum.textContent) {
-      if (num1 && !num2) {
-        num2 = parseInt(b);
-        array[1] = num2;
-      } else if (!num1) {
-        num1 = parseInt(a);
-        array[0] = num1;
-      }
+    arrayOp[0] = operatorBtn.value;
+    if (isLastInputOperator == true) {
+      let inputNumArray = inputNum.textContent.split("");
+      inputNumArray[inputNumArray.length - 1] = operatorBtn.value;
+      inputNum.textContent = inputNumArray.join("");
+    } else if (inputNum.textContent.length != 0 && operatorBtn.value != "=") {
+      inputNum.textContent += operatorBtn.value;
+    } else if (inputNum.textContent.length == 0 && operatorBtn.value != "=") {
+      inputNum.textContent = "0";
+      inputNum.textContent += operatorBtn.value;
+    } else if (inputNum.textContent.length == 0 && operatorBtn.value == "=")
+      return;
+    isLastInputOperator = true;
+    if (!num1) {
+      num1 = a;
+      console.log(num1);
+      array[0] = num1;
     }
-    arrayOp.push(operatorBtn.value);
-    console.log(array.length);
-    if (array.length > 1) {
-      operate();
-      num1 = parseInt(result.textContent);
-      num2 = null;
+    if (result.textContent) {
+      num1 = result.textContent;
       b = "";
+      num2 = null;
       array = [num1];
-      arrayOp = [];
-      console.log(array);
+      console.log(num1);
     }
-    if (operatorBtn.value == "=" && inputNum.textContent) {
-      inputNum.textContent = "";
-      a = "";
-      b = "";
-      num1 = null;
-      num2 = null;
-      array = [];
-      arrayOp = [];
-      numBtns.forEach((numBtn) => {
-        numBtn.addEventListener("click", () => {
-          result.textContent = "";
-        });
-      });
-    }
+    console.log(arrayOp);
+
+    // arrayOp.push(operatorBtn.value);
+    // console.log(array.length);
+    // if (array.length > 1) {
+    //   operate();
+    //   num1 = parseFloat(result.textContent);
+    //   num2 = null;
+    //   b = "";
+    //   array = [num1];
+    //   arrayOp.splice(0, 1);
+    //   console.log(array);
+    // }
+    // if (operatorBtn.value == "=" && inputNum.textContent) {
+    //   // inputNum.textContent = "";
+    //   a = "";
+    //   b = "";
+    //   num1 = null;
+    //   num2 = null;
+    //   array = [];
+    //   arrayOp = [];
+    //   numBtns.forEach((numBtn) => {
+    //     numBtn.addEventListener("click", () => {
+    //       result.textContent = "";
+    //     });
+    //   });
+    // }
     if (operatorBtn.value == "clear") {
-      a = "";
+      isLastInputOperator = false;
+      a = "0";
       b = "";
       num1 = null;
       num2 = null;
@@ -170,16 +113,16 @@ operatorBtns.forEach((operatorBtn) => {
 function operate() {
   switch (arrayOp[0]) {
     case "+":
-      result.textContent = add(num1, num2);
+      result.textContent = Math.round(add(num1, num2) * 1000) / 1000;
       break;
     case "-":
-      result.textContent = subtract(num1, num2);
+      result.textContent = Math.round(subtract(num1, num2) * 1000) / 1000;
       break;
     case "*":
-      result.textContent = multiply(num1, num2);
+      result.textContent = Math.round(multiply(num1, num2) * 1000) / 1000;
       break;
     case "/":
-      result.textContent = divide(num1, num2);
+      result.textContent = Math.round(divide(num1, num2) * 1000) / 1000;
       break;
   }
 }
